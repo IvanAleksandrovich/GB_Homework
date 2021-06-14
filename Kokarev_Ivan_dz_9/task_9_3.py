f9fc07144b645dfb8c8e494551a6fa3adb99a596
Ivan <KokarevIA@yandex.ru>
2021-06-14 22:56:33 +0300

    Add homework 9

---
+++ Kokarev_Ivan_dz_9/task_9_3.py
@@ -0,0 +1,19 @@
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name, self.surname, self. position, self._income = name, surname, position, {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return f"{self.name} {self.surname}"
+
+    def get_total_income(self):
+        return f"{self._income['wage'] + self._income['bonus']}"
+
+
+data_work = Position("Ivan", "Kokarev", "car mechanic", 40000, 10000)
+print(data_work.get_full_name())
+print(data_work.get_total_income())
